ac9b0c3ad25c344593d19abee307f6fea724aa96
bakjuna <bakjuna@gmail.com>
2021-08-28 23:35:20 +0900

    adding modals

--- components/AddDashboardModal.tsx
+++ components/AddDashboardModal.tsx
@@ -1,34 +1,110 @@
-import { Modal } from 'antd'
-import { useState } from 'react';
+import { Modal, List } from 'antd'
+import { useEffect, useRef, useState } from 'react';
 
-const AddDashboardModal = ({visible, setVisible}) => {
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const [modalText, setModalText] = useState('ㄸㅆㄸㅆ');
+interface AddDashboardModalInterface {
+	visible: boolean
+	setVisible: (visible: boolean) => void
+}
 
+interface DashboardListStyle {
+	display: string
+}
+const AddDashboardModal = ({visible, setVisible}: AddDashboardModalInterface) => {
+	const [confirmLoading, setConfirmLoading] = useState<boolean>(false)
+	const [listStyle, setListStyle]= useState<DashboardListStyle>({ display: 'initial' })
+	const [dashboardSelectStyle, setDashboardSelectStyle]= useState<DashboardListStyle>({ display: 'none' })
+	const [selectedAid, setSelectedAid] = useState<string>('')
+	const listRef = useRef(null)
+	const contentRef = useRef(null)
 
-  const handleOk = () => {
-    setConfirmLoading(true);
-    setVisible(false);
-    setConfirmLoading(false);
-  };
+	const dashboards = [
+		{
+			title: '캘린더',
+			aid: 'calander'
+		},
+		{
+			title: '계산기',
+			aid: 'calculator'
+		}
+	]
 
-  const handleCancel = () => {
-    setVisible(false)
-  };
+	const sizes = [
+		{
+			title: '대형'
+		},
+		{
+			title: '중형'
+		},
+		{
+			title: '소형'
+		},
+	]
+	const handleOk = () => {
+		setConfirmLoading(true);
+		setVisible(false);
+		setConfirmLoading(false);
+	};
 
-  return (
-    <>
-      <Modal
-        title="대시보드"
-        visible={visible}
-        onOk={handleOk}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
-      >
-        <p>{modalText}</p>
-      </Modal>
-    </>
-  )
+	const handleCancel = () => {
+		setVisible(false)
+	};
+	const onClickDashboardTypesHandler = (aid: string) => {
+		setListStyle({display: 'none'})
+		setDashboardSelectStyle({display: 'initial'})
+		setSelectedAid(aid)
+	}
+
+	const onClickSelectDashboardHandler = () => {
+
+	}
+
+	return (
+		<>
+			<Modal
+				title="대시보드"
+				visible={visible}
+				onOk={handleOk}
+				confirmLoading={confirmLoading}
+				onCancel={handleCancel}
+			>
+				<div style={listStyle} ref={listRef}>
+					<List
+						itemLayout="horizontal"
+						dataSource={dashboards}
+						renderItem={item => (
+							<List.Item
+								style={{cursor:'pointer'}}
+								onClick={() => onClickDashboardTypesHandler(item.aid)}
+							>
+							<List.Item.Meta
+								title={<a>{item.title}</a>}
+							/>
+						</List.Item>
+						)}
+					>
+					</List>
+				</div>
+				<div style={dashboardSelectStyle} ref={contentRef}>
+
+					<List
+						itemLayout="horizontal"
+						dataSource={sizes}
+						renderItem={item => (
+							<List.Item
+							style={{cursor:'pointer'}}
+								onClick={() => onClickSelectDashboardHandler()}
+							>
+							<List.Item.Meta
+								title={<a href="/">{item.title}</a>}
+							/>
+						</List.Item>
+						)}
+					>
+					</List>
+				</div>
+			</Modal>
+		</>
+	)
 }
 
 export default AddDashboardModal
--- components/MainSettingsModal.tsx
+++ components/MainSettingsModal.tsx
@@ -2,33 +2,33 @@ import { Modal } from 'antd'
 import { useState } from 'react';
 
 const MainSettingsModal = ({visible, setVisible}) => {
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const [modalText, setModalText] = useState('Cㅆ');
+	const [confirmLoading, setConfirmLoading] = useState(false);
+	const [modalText, setModalText] = useState('Cㅆ');
 
 
-  const handleOk = () => {
-    setConfirmLoading(true);
-    setVisible(false);
-    setConfirmLoading(false);
-  };
+	const handleOk = () => {
+		setConfirmLoading(true);
+		setVisible(false);
+		setConfirmLoading(false);
+	};
 
-  const handleCancel = () => {
-    setVisible(false)
-  };
+	const handleCancel = () => {
+		setVisible(false)
+	};
 
-  return (
-    <>
-      <Modal
-        title="세팅"
-        visible={visible}
-        onOk={handleOk}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
-      >
-        <p>{modalText}</p>
-      </Modal>
-    </>
-  )
+	return (
+		<>
+			<Modal
+				title="세팅"
+				visible={visible}
+				onOk={handleOk}
+				confirmLoading={confirmLoading}
+				onCancel={handleCancel}
+			>
+				<p>{modalText}</p>
+			</Modal>
+		</>
+	)
 }
 
 export default MainSettingsModal
