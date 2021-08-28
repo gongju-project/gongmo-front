62920f40531854410bbc58ddcd9aecf699e5d613
bakjuna <bakjuna@gmail.com>
2021-08-28 16:11:25 +0900

    redirect url added

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+	env: {
+		GONGMO_API_HOST: 'https://apis.finwhale.io',
+	},
+}
+  
--- pages/signIn/index.tsx
+++ pages/signIn/index.tsx
@@ -3,39 +3,42 @@ import HeaderCustom from '../../utils/header'
 import { Col, Layout, Row } from 'antd'
 import 'antd/dist/antd.css'
 import * as React from 'react'
+import { useRouter } from 'next/router'
 
-const onKaKaoClickLogInHandler = () => {
 
-}
-const onGoogleClickLogInHandler = () => {
-
-}
 const Main = () => {
-	return <>
-	<Layout>
+    const router = useRouter()
+    const onKaKaoClickLogInHandler = () => {
+        router.push(process.env.GONGMO_API_HOST + '/kakaoOAuth/login')
+    }
+    const onGoogleClickLogInHandler = () => {
+        router.push(process.env.GONGMO_API_HOST + '/googleOAuth/login')
+    }
+    return <>
+        <Layout>
             <HeaderCustom
-                currentMenuKey = {3}
+                currentMenuKey={3}
             ></HeaderCustom>
             <Layout className={styles.signInContent}>
                 <Row>
                     <Col span={8}>
                     </Col>
                     <Col span={8}>
-                    <img className={styles.loginLogo} src='/logo/logo.png' />
-                    <div>
-                        <button className={`${styles.loginBtnKakao} ${styles.socialLoginBtn}`} onClick={() => onKaKaoClickLogInHandler()} >카카오 로그인</button>
-                    </div>
-                    <div>
-                        <button className={`${styles.loginBtnGoogle} ${styles.socialLoginBtn}`} onClick={() => onGoogleClickLogInHandler()} >구글 로그인</button>
-                    </div>
+                        <img className={styles.loginLogo} src='/logo/logo.png' />
+                        <div>
+                            <button className={`${styles.loginBtnKakao} ${styles.socialLoginBtn}`} onClick={() => onKaKaoClickLogInHandler()} >카카오 로그인</button>
+                        </div>
+                        <div>
+                            <button className={`${styles.loginBtnGoogle} ${styles.socialLoginBtn}`} onClick={() => onGoogleClickLogInHandler()} >구글 로그인</button>
+                        </div>
                     </Col>
                     <Col span={8}>
                     </Col>
                 </Row>
-		</Layout>
-	</Layout>
+            </Layout>
+        </Layout>
 
-	</>
+    </>
 }
 
 export default Main
