1f7b02b7cb6e3a09dec9848eee891f849faa3a28
bakjuna <bakjuna@gmail.com>
2021-08-29 17:07:45 +0900

    api host corrected

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-	env: {
-		GONGMO_API_HOST: 'http://127.0.0.1:3000',
-	},
-}
-  
+export const env = {
+	GONGMO_API_HOST: 'https://apis.finwhale.io',
+};
